0dd523fead16825139b48992cee7b23d3a0c649a
jpantonow <jpantonow@gmail.com>
2024-11-10 06:11:07 -0300

    bitcoin master??

--- submission/003.sh
+++ submission/003.sh
@@ -1,6 +1,5 @@
 # How many new outputs were created by block 123,456?
 
 hash=$(bitcoin-cli getblockhash 123456)
-data=$(bitcoin-cli getblock $hash 2)
-counter=$(jq '[.tx[].vout | length] | add')
-outputs_count=$(bitcoin-cli $data | $counter)
+data=$(bitcoin-cli getblock "$hash" 2)
+counter=$(echo "$data" | jq '[.tx[].vout | length] | add')
