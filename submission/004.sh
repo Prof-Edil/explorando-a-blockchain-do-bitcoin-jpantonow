211070ebdca2b0eb2c531a62da65aac9ec929f91
jpantonow <jpantonow@gmail.com>
2025-01-04 20:04:22 -0300

    some refs

--- submission/004.sh
+++ submission/004.sh
@@ -3,6 +3,6 @@
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 INDEX=100
 DESCRIPTOR="tr(${XPUB}/0/*)"
-bitcoin-cli importdescriptors "[{\"desc\":\"${DESCRIPTOR}\",\"active\":true,\"internal\":false,\"timestamp\":\"now\"}]"
-DERIVED_DESC=$(bitcoin-cli deriveaddresses "tr(${XPUB}/0/${INDEX})")
-echo "$DERIVED_DESC"
+DERIVATION="tr(${XPUB}/0/${INDEX})"
+RESULT=$(bitcoin-cli deriveaddresses "$DERIVATION")
+echo "$RESULT"
