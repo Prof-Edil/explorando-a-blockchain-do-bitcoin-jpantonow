ee4e72ad05ccbbdf4648ce8c147b3fa2c329dd80
jpantonow <jpantonow@gmail.com>
2025-01-04 20:02:30 -0300

    some refs

--- submission/004.sh
+++ submission/004.sh
@@ -3,6 +3,6 @@
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 INDEX=100
 DESCRIPTOR="tr(${XPUB}/0/*)"
-IMPORT_RESULT=$(bitcoin-cli importdescriptors "[{\"desc\":\"${DESCRIPTOR}\",\"active\":true,\"internal\":false,\"timestamp\":\"now\"}]")
-ADDRESS=$(bitcoin-cli getnewaddress "" "bech32m" | jq -r '.address')
-echo "$ADDRESS"
+bitcoin-cli importdescriptors "[{\"desc\":\"${DESCRIPTOR}\",\"active\":true,\"internal\":false,\"timestamp\":\"now\"}]"
+DERIVED_DESC=$(bitcoin-cli deriveaddresses "tr(${XPUB}/0/${INDEX})")
+echo "$DERIVED_DESC"
