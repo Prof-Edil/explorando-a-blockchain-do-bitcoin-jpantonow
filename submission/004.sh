b221d50a29a17d33ac71feab51641110970a5155
jpantonow <jpantonow@gmail.com>
2025-01-04 19:47:23 -0300

    some refs

--- submission/004.sh
+++ submission/004.sh
@@ -1,3 +1,11 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
-bitcoin-cli -rpcwallet="mywallet" deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/100)"
+WALLET_NAME="mywallet"
+XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+INDEX=100
+
+bitcoin-cli -named createwallet wallet_name="$WALLET_NAME" descriptors=true
+
+bitcoin-cli -rpcwallet="$WALLET_NAME" importdescriptors "[{\"desc\":\"tr($XPUB/$INDEX)\",\"timestamp\":\"now\",\"active\":true,\"range\":[$INDEX,$INDEX]}]"
+
+bitcoin-cli -rpcwallet="$WALLET_NAME" deriveaddresses "tr($XPUB/$INDEX)"
