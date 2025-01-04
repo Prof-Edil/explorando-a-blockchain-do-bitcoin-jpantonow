9e3e2de6e8df9907cdb076cd3b89f004ab2b2f22
jpantonow <jpantonow@gmail.com>
2025-01-04 19:59:35 -0300

    some refs

--- submission/004.sh
+++ submission/004.sh
@@ -1,13 +1,8 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
-WALLET_NAME="mywallet"
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 INDEX=100
-
-bitcoin-cli -named createwallet wallet_name="$WALLET_NAME" descriptors=true
-
-bitcoin-cli -rpcwallet="$WALLET_NAME" importdescriptors "[{\"desc\":\"tr($XPUB/$INDEX)\",\"timestamp\":\"now\",\"active\":true,\"range\":[$INDEX,$INDEX]}]"
-
-ADDRESS=$(bitcoin-cli -rpcwallet="$WALLET_NAME" deriveaddresses "tr($XPUB/$INDEX)")
-
+DESCRIPTOR="tr(${XPUB}/0/*)"
+IMPORT_RESULT=$(bitcoin-cli importdescriptors "[{\"desc\":\"${DESCRIPTOR}\",\"active\":true,\"internal\":false,\"timestamp\":\"now\"}]")
+ADDRESS=$(bitcoin-cli getnewaddress "" "bech32m" | jq -r '.address')
 echo "$ADDRESS"
