0a0bf1040185e34096e7404cabb0093be9932b35
jpantonow <jpantonow@gmail.com>
2025-01-12 13:43:19 -0300

    006?

--- submission/006.sh
+++ submission/006.sh
@@ -1,12 +1,17 @@
-for TRANSACTION_ID in $(bitcoin-cli getblock $(bitcoin-cli getblockhash 257343) | jq -r .tx[])
+TRANSACTION_LIST=$(bitcoin-cli getblock $(bitcoin-cli getblockhash 257343)| jq -r .tx[])
+COINBASE_TRANSACTION_ID=$(bitcoin-cli getblock $(bitcoin-cli getblockhash 256128) | jq -r .tx[0])
+COINBASE_OUTPUT_INDEX=0
+
+for TRANSACTION_ID in $TRANSACTION_LIST
 do
-    readarray -t TX_INPUTS < <(bitcoin-cli getrawtransaction $TRANSACTION_ID 1 | jq -c .vin[])
+    readarray -t TRANSACTION_INPUTS < <(bitcoin-cli getrawtransaction $TRANSACTION_ID 1 | jq -c .vin[])
     
-    for TX_INPUT in "${TX_INPUTS[@]}"
+    for INPUT_DATA in "${TRANSACTION_INPUTS[@]}"
     do
-        PREV_TXID=$(echo "$TX_INPUT" | jq -r .txid)
-        PREV_VOUT=$(echo "$TX_INPUT" | jq -r .vout)
-        if [[ $(bitcoin-cli getblock $(bitcoin-cli getblockhash 256128) | jq -r .tx[0]) == $PREV_TXID && $(0) == $PREV_VOUT ]]
+        PREVIOUS_TRANSACTION_ID=$(echo "$INPUT_DATA" | jq -r .txid)
+        PREVIOUS_OUTPUT_INDEX=$(echo "$INPUT_DATA" | jq -r .vout)
+        
+        if [[ $COINBASE_TRANSACTION_ID == $PREVIOUS_TRANSACTION_ID && $COINBASE_OUTPUT_INDEX == $PREVIOUS_OUTPUT_INDEX ]]
         then
             echo $TRANSACTION_ID
             exit 0
